babf2eae979f72260ecdab145159d19dbf64ba74
Hamza Khan <112khanhamza@gmail.com>
2021-07-02 02:57:33 +0530

    Fixed URL's for Name it and Portfolio Project

--- src/assets/projectsData.js
+++ src/assets/projectsData.js
@@ -6,7 +6,7 @@ export const projectsData = [
         imageUrl: 'https://madhavbahl.tech/portfolio-assets/proj1.PNG',
         videoUrl:
             'https://www.youtube.com/playlist?list=PLGyA74h_S9NqJvvQ7-l9bUHHS6bMCkgo0',
-        projectUrl: 'http://theleanprogrammer.com/name-it/',
+        projectUrl: 'https://112khanhamza.github.io/nameit/',
     },
     {
         projectName: 'FlashType',
@@ -41,6 +41,6 @@ export const projectsData = [
             'As a developer/designer, you should definitely have your own portfolio website. There are endless benefits. But, a lot of people feel that it would be tough to make one. So here I bring you a tutorial to make your own website.',
         imageUrl: 'https://madhavbahl.tech/portfolio-assets/proj5.PNG',
         videoUrl: 'http://theleanprogrammer.com/aam',
-        projectUrl: 'http://madhavbahl.tech/portfolio/',
+        projectUrl: 'https://112khanhamza.github.io/portfolio/',
     },
 ];
